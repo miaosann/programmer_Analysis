777f3e4ae557f25d4bc9e353d06fdbb25882ae33
miaosann <miaohualin2016@163.com>
2018-12-10 16:55:25 +0800

    spider is completed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # programmer_Analysis
 基于Github来分析程序员对软件项目质量的影响
+
+- ##### 建立爬虫爬数据
+
+  首先实现了`github_spider.py`进行爬取，但是由于单线程爬取速度太慢，所有进行改进，为`mul_github_spider.py`多线程爬取。
---
+++ venv/github_spider.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+try:
+    from functools import namedtuple
+except ImportError:
+    from collections import namedtuple
+
+HEADER = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
+}
+COLUMNS = ['user', 'name', 'position', 'repositories', 'stars', 'followers', 'following', 'contributions']
+PROFILE = namedtuple('PROFILE', COLUMNS)
+Result = []
+DF = pd.DataFrame(columns=COLUMNS, index=["0"])
+
+def _str_2_int(stri):
+    if 'k' in stri:
+        return int(float(stri[:-1]) * 1000)
+    if ',' in stri:
+        return int(stri.replace(',', ''))
+    else:
+        return int(stri)
+
+
+def user_crawler(user):
+    url = 'https://github.com/{}'.format(user)
+    values = [None] * len(COLUMNS)
+    values[COLUMNS.index('user')] = user
+    try:
+        html = requests.get(url, headers=HEADER, timeout=10).text
+        soup = BeautifulSoup(html, 'lxml')
+
+        tag_name = soup.find_all('span', class_='p-name vcard-fullname d-block')
+        if len(tag_name) > 0:
+            name = tag_name[0].text
+            if len(name) > 0:
+                values[COLUMNS.index('name')] = name
+
+        tag_position = soup.find_all('span', class_='p-label')
+        if len(tag_position) > 0:
+            position = tag_position[0].text
+            values[COLUMNS.index('position')] = position
+
+        tags_overview = soup.find_all('span', class_='Counter')
+        repositories = _str_2_int(tags_overview[0].text.replace('\n', '').replace(' ', ''))
+        stars = _str_2_int(tags_overview[1].text.replace('\n', '').replace(' ', ''))
+        followers = _str_2_int(tags_overview[2].text.replace('\n', '').replace(' ', ''))
+        following = _str_2_int(tags_overview[3].text.replace('\n', '').replace(' ', ''))
+        values[COLUMNS.index('repositories')] = repositories
+        values[COLUMNS.index('stars')] = stars
+        values[COLUMNS.index('followers')] = followers
+        values[COLUMNS.index('following')] = following
+
+        tag_contributions = soup.find_all('h2', class_='f4 text-normal mb-2')
+        try:
+            contributions = _str_2_int(
+                tag_contributions[0].text.replace('\n', '').replace(' ', '').replace('contributionsinthelastyear', ''))
+        except Exception as err:
+            contributions = _str_2_int(
+                tag_contributions[0].text.replace('\n', '').replace(' ', '').replace('contributioninthelastyear', ''))
+        values[COLUMNS.index('contributions')] = contributions
+
+        print(values)
+        Result.append(values)
+    except Exception as e:
+        print(e)
+
+
+def get_all_followers(user):
+    followers_list = []
+    idx = 0
+    url = 'https://github.com/{}?tab=followers'
+    while True:
+        idx += 1
+        # page_url = url.format(user, idx)
+        if idx > 1:
+            page_url = url
+        else:
+            page_url = url.format(user)
+
+        print("PAGE_URL：" + page_url)
+        try:
+            html = requests.get(page_url, headers=HEADER, timeout=10).text
+            # print("HTML："+html)
+            if '<span class="disabled">Next</span>' in html:
+                break
+            soup = BeautifulSoup(html, 'lxml')
+            tag_names = soup.find_all('span', class_='link-gray pl-1')
+            for name in tag_names:
+                followers_list.append(name.text)
+            if '<div class="paginate-container">' not in html:
+                break
+            nextUrl = soup.find('div', class_='pagination').children
+            for urlItem in nextUrl:
+                if urlItem.string == "Next":
+                    # print(urlItem.attrs['href'])
+                    url = urlItem.attrs['href']
+                    # print("hh："+url)
+
+        except Exception as e:
+            print(e)
+    return followers_list
+
+
+def save():
+    with open("result.csv", "w+" ,encoding='utf-8' ,newline='') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(COLUMNS)
+        f_csv.writerows(Result)
+        print('data saved')
+
+
+followers_list = []
+
+
+def main():
+    main_user = ''
+    print('Let us begin ...')
+    followers_list = get_all_followers(main_user)
+    for user in followers_list:
+        user_crawler(user)
+    print(Result)
+    save()
+
+
+if __name__ == '__main__':
+    main()
